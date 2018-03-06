48f82d43f772ce84550c341851ac7536a9b7c45f
cyokodog <cyokodog@gmail.com>
2018-03-06 17:27:15 +0900

    login user名を表示

--- src/components/account/Account.vue
+++ src/components/account/Account.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
-    <img v-bind:src="auth.photoURL" v-bind:alt="auth.displayName" @click="onChangeUser">
+    <a class="userInfo" @click="onChangeUser" href="#">
+      <span class="name">{{auth.displayName}}</span><img v-bind:src="auth.photoURL">
+    </a>
   </div>
 </template>
+
+<style scoped>
+.userInfo {
+  color: #00aaff;
+}
+.userInfo > * {
+  vertical-align: middle;
+  line-height: 1;
+}
+.name {
+  margin-right: 16px;
+}
+</style>
 
 <script>
 export default require('./index.js')
