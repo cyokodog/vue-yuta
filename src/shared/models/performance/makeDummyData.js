bc91a457c95f7eab0dbf24aa688cb58558b7448c
cyokodog <cyokodog@gmail.com>
2018-03-06 16:59:03 +0900

    初期データ修正

--- src/shared/models/performance/makeDummyData.js
+++ src/shared/models/performance/makeDummyData.js
@@ -10,6 +10,52 @@ const data = [
     type: 'good'
   },
   {
+    name: '漢検5級合格',
+    date: '2018/03/04',
+    type: 'good'
+  },
+
+  {
+    name: '部屋のかたずけ忘れ',
+    date: '2018/02/20',
+    type: 'bad'
+  },
+  {
+    name: '部屋のかたずけ忘れ',
+    date: '2018/02/21',
+    type: 'bad'
+  },
+  {
+    name: 'うがい手洗い忘れ',
+    date: '2018/02/22',
+    type: 'bad'
+  },
+  {
+    name: 'うがい手洗い忘れ',
+    date: '2018/02/23',
+    type: 'bad'
+  },
+  {
+    name: '布団たたみ忘れ',
+    date: '2018/02/24',
+    type: 'bad'
+  },
+  {
+    name: '上履き干し忘れ',
+    date: '2018/02/24',
+    type: 'bad'
+  },
+  {
+    name: 'やる事やる前に遊ぶ',
+    date: '2018/02/25',
+    type: 'bad'
+  },
+  {
+    name: '塾の後遊んで帰ってこない',
+    date: '2018/03/01',
+    type: 'bad'
+  },
+  {
     name: 'カギのし忘れ',
     date: '2018/02/25',
     type: 'bad'
@@ -80,18 +126,18 @@ const data = [
     type: 'bad'
   },
   {
-    name: 'ちこく',
+    name: '寝坊してちこく',
     date: '2018/02/26',
     type: 'bad'
   },
   {
-    name: 'ちこく',
-    date: '2018/02/27',
+    name: '寝坊してちこく',
+    date: '2018/03/02',
     type: 'bad'
   },
   {
-    name: 'ちこく',
-    date: '2018/02/28',
+    name: '寝坊してちこく',
+    date: '2018/02/20',
     type: 'bad'
   }
 ]
