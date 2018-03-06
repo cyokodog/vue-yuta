c8246e9a64cd4dda8ba4733882be655af12dbecc
cyokodog <cyokodog@gmail.com>
2018-03-06 10:06:52 +0900

    読み込み失敗することがあるので、App.jsをApp.vueに内包させる

--- src/App.vue
+++ src/App.vue
@@ -50,5 +50,25 @@
 </style>
 
 <script>
-export default require('./App.js')
+import Account from './components/account/Account'
+
+export default {
+  name: 'App',
+  components: {
+    Account
+  },
+  computed: {
+    auth () {
+      return this.$store.state.auth
+    },
+    performances () {
+      return this.$store.state.db.performances
+    }
+  },
+  mounted () {
+    this.$store.commit('signIn')
+    this.$store.commit('prepareDb')
+    // this.$store.commit('makeDummyData')
+  }
+}
 </script>
